170906bf9c0a20e557c349095380fd143656b6e5
Andres Castellanos <andres.about@gmail.com>
2024-09-26 09:56:40 -0500

    setting up cva and cn utility functions

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -17,5 +17,7 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     label: "Button",
+    intent: "primary",
+    size: "medium",
   },
 };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,36 @@
+import { cn } from "@/utils";
+import { cva, VariantProps } from "class-variance-authority";
 import React from "react";
 
-function Button({ label }: { label: string }) {
+const button = cva("button", {
+  variants: {
+    intent: {
+      primary: ["bg-blue-500"],
+      secondary: ["bg-white"],
+    },
+    size: {
+      small: ["text-sm", "py-1", "px-2"],
+      medium: ["text-base", "py-2", "px-4"],
+    },
+  },
+  compoundVariants: [{ intent: "primary", size: "medium", class: "uppercase" }],
+  defaultVariants: {
+    intent: "primary",
+    size: "medium",
+  },
+});
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof button> {
+  label: string;
+}
+
+function Button({ label, intent, size, className, ...delegate }: ButtonProps) {
   return (
-    <div className="bg-primary-400 text-white rounded-sm p-6">{label}</div>
+    <button className={cn(button({ intent, size, className }))} {...delegate}>
+      {label}
+    </button>
   );
 }
 
