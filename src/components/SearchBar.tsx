b1ab5d35e48f17225f22413a3a4a06045de83b27
Andres Castellanos <andres.about@gmail.com>
2024-09-29 12:31:14 -0500

    searchBar padding tweak

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -29,12 +29,12 @@ function SearchBar({ categories, className }: SearchBarProps) {
   return (
     <section
       className={cn(
-        "flex h-11 items-center rounded-full max-w-[563px] border border-muted-300 bg-white",
+        "flex items-center rounded-full max-w-[563px] border border-muted-300 bg-white pl-5",
         className,
       )}
     >
       <div className="flex items-center grow h-full">
-        <Search strokeWidth={3} className="size-4 text-muted-400 mx-3 " />
+        <Search strokeWidth={3} className="size-4 text-muted-400 mr-3 " />
         <input
           className="placeholder:text-muted-500 grow text-sm h-full inline-block min-w-[20.4rem]"
           type="text"
@@ -43,7 +43,7 @@ function SearchBar({ categories, className }: SearchBarProps) {
       </div>
       <Dropdown
         itemsClassName="min-w-[174px] py-2.5 px-4"
-        className="min-w-[194px] bg-muted-400 py-3 px-5 rounded-e-full text-white text-sm data-[state=open]:bg-[#EBEFF4] data-[state=open]:text-muted-500"
+        className="min-w-[194px] bg-muted-400 py-4 px-5 rounded-e-full text-white text-sm data-[state=open]:bg-[#EBEFF4] data-[state=open]:text-muted-500"
         items={categories}
         onValueChange={handleDropdownClick}
       >
